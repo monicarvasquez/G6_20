afcc8f519c9de45a883557eb645256a52c439e49
paolarodz <ypfloresr@unah.hn>
2023-03-24 23:57:03 -0600

    Obtener_1 Registro_Avion

--- controllers/avion_controller.js
+++ controllers/avion_controller.js
@@ -25,4 +25,30 @@ var AvionModel = require('../models/avion_model'),
     })
  }
 
+ AvionController.getOne=(req, res, next)=> {
+    let num_avion= req.body.num_avion
+    console.log(num_avion)
+
+    AvionModel.getOne(num_avion,(err,rows)=> {
+    console.log(err,'---',rows)
+    if(err)
+    {
+        let locals={
+            title: `Error al consultar la base de datos con el Id: ${num_avion}`,
+            descripcion: 'Error de Sintaxis SQL',
+            error: err
+        }
+        res.render('error',locals)
+    }
+    else
+    {
+        let locals ={
+            title:'num_Avion encontrado',
+            data:rows
+        }
+        res.status(200).send(rows.rows)
+    }
+    })
+}
+
  module.exports=AvionController;
